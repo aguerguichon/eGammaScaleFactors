cf6d7fb8ab7d5196be6439e9113ea3d994f9e73a
Christophe Goudet <goudet@lal.in2p3.fr>
2015-03-17 18:39:59 +0100

    set right etaCalo

--- prun.sh
+++ prun.sh
@@ -1,18 +1,19 @@
 #input=data12_8TeV.periodB.physics_Egamma.PhysCont.AOD.repro16_v05/
-input=data12_8TeV.00204158.physics_Egamma.merge.DAOD_ZEE.r4065_p1346_r6469_p2298/
-#input=mc14_8TeV.129680.PowhegPythia8_AU2CT10_Zee_DiLeptonFilter.merge.AOD.e1861_s1933_s1911_r5591_r5625/ 
-#input=mc14_8TeV.129680.PowhegPythia8_AU2CT10_Zee_DiLeptonFilter.merge.DAOD_EGAM1.e1861_s1933_s1911_r5591_r5625_p1796/
+#input=data12_8TeV.00204158.physics_Egamma.merge.DAOD_ZEE.r4065_p1346_r6469_p2298/
 #input=mc14_8TeV.147806.PowhegPythia8_AU2CT10_Zee.merge.AOD.e1169_s1896_s1912_r5591_r5625/
 #input=mc14_8TeV.147806.PowhegPythia8_AU2CT10_Zee.merge.DAOD_EGAM1.e1169_s1896_s1912_r5591_r5625_p1796/
+input=data12_8TeV.periodAllYear.physics_Egamma.PhysCont.DAOD_ZEE.repro16_v02
 
 outFileName=ZeeNtuple.root
-version=2
+version=19
 doScale=0
 doSmearing=0
 options=""
 #Do not put usercgoudet nor version
-outDatasetName="data_8TeV_DZee_rel20"
+outDatasetName=""
 #MC_8TeV_DZee"
+#data_8TeV_DZee_rel20"
+
 
 #=============================
 if [[ ${outDatasetName} == "" ]];
