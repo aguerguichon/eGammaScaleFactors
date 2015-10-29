8ab16e638ab5434b49e30e14f1a9c61a2128e912
Christophe Goudet <goudet@lal.in2p3.fr>
2015-10-29 15:44:29 +0100

    download works

--- ZeeAnalysis/python/RunGrid.py
+++ ZeeAnalysis/python/RunGrid.py
@@ -16,6 +16,8 @@ if len( sys.argv ) == 1 :
     print 'DATA50 to update 50ns datasets'
     print 'MC25'
 
+launcherPath = os.popen( 'pwd' ).read().split()[0]+'/'
+
 for option in sys.argv:
     if option == 'RunGrid.py' : continue
 
@@ -88,14 +90,61 @@ for option in sys.argv:
         GetDataFiles( inputs, 0, doScale, 2, electronID, 3, outFilePrefix, esModel, 27, ptCutVect)
         GetDataFiles( inputs, 0, doScale, 1, electronID, 3, outFilePrefix, esModel, 27, ptCutVect)
 
+##==================================================================
     if option=='download' :
+        missingFiles=[]
         path='/sps/atlas/c/cgoudet/Calibration/DataxAOD/'
+
+#Get the list of jobs and datasets from the txt file
         gridJobIDList = np.genfromtxt( 'GridJobList.txt', dtype='S100', delimiter=' ' )
-        for job in gridJobIDList :
-            print job
+        toRemove=[]
+        for job in gridJobIDList : 
+            if job[1] == 'Test' : continue #First line is used to be sure to have a 2D vector
+            line='http://bigpanda.cern.ch/task/\?jeditaskid\='+ job[0] + '\&display_limit\=200'
 
-# mode = 1
+#read the web page were the info oof the jobs are stored
+            output = os.popen( 'curl '+ line).read().split('\n')
+            status=''
+            for htmlLine in output :
+#check if the job is in the 'done' state
+                if 'class=\'done_fill\'' in htmlLine : 
+                    status='done'
+                    break
 
+#create the name of the repository were the dataset must be downloaded
+            directory = job[1].replace( 'user.cgoudet.', '' ).replace('_Ntuple.root/', '' )
+            directory = directory[:directory.rfind('_')]
+            os.chdir( path + directory )
+
+#download the dataset with rucio and get the number of failed download
+            nMiss = os.popen( 'rucio download '+ job[1] ).read().split()[-1]
+            os.system('rm OK*')
+
+#if done and missing files print it and do noting 
+# if done and no missing file mark the job for removal and print and OK file in the directory
+            if status == 'done' :
+                if not int(nMiss)  :
+                    os.system('touch OK' + job[1].split('.')[2].split('_')[-2] )
+                    addFileLine= 'AddFile ' + job[1] + '* --outName ' + directory + '.root'
+                    print addFileLine
+                    toRemove.append( job[0] )
+                else :
+                    missingFiles.append( [ dataset, nMiss ] )
+            pass#end status condition
+        pass#end job loop
+
+#cannot delete from gridJobIDList so I have to create a new one and add only interesting bins
+    newGridJobList=[]
+    for job in gridJobIDList : 
+        if job[0] in toRemove : continue
+        newGridJobList.append( job )
+
+    print newGridJobList
+    os.chdir( launcherPath )
+    np.savetxt( 'GridJobList2.txt', newGridJobList, delimiter=' ', fmt='%s')   
+
+
+#===================================================
 if len( inputs ) :
 
     if len( inputs ) != len( doScale ) or len( inputs ) != len( outFilePrefix ) or len( inputs ) != len( esModel ) :
@@ -105,7 +154,7 @@ if len( inputs ) :
     tab = np.genfromtxt( "Version.txt", dtype='S100', delimiter=' ' )
 
     jobIDList=[]
-#=============================
+
     for iFile in range( 0, len( inputs ) ) :
         version=0
         
@@ -149,7 +198,8 @@ if len( inputs ) :
         result = sub.check_output([commandLine], shell=1, stderr=sub.STDOUT)    
         for line in result.split() :
             if 'jediTaskID' in line : 
-                jobIDList.append( [line.split('=')[1], 'user.cgoudet.' + outFileName+'_Ntuple.root' )
+                jobIDList.append( [line.split('=')[1], 'user.cgoudet.' + outFileName+'_Ntuple.root'] )
+        pass
     np.savetxt( 'Version.txt', tab, delimiter=' ', fmt='%s')   
     with open( 'GridJobList.txt', 'a') as jobFile :
         for job in jobIDList : jobFile.write( job[0] + ' ' + job[1] + '\n' )
